4c099818e4a3f2bc1c29811b0e44f9b1efca1f05
LokmanHossen <lokmanhossen230@gmail.com>
2023-11-09 23:27:56 +0600

    Welcome Page

---
+++ lib/main.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+import 'package:whats_app_clone/feature/welcome/pages/welcome_page.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return  MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'What\'sApp',
+      theme: ThemeData.dark(),
+      home: const WelcomePage(),
+    );
+  }
+}
