99be2f5912e4e8ff1ef0c0c3a0ffd467a5e59e17
LokmanHossen <lokmanhossen230@gmail.com>
2023-11-11 01:56:16 +0600

    Add Light Theme

--- lib/common/theme/light_theme.dart
+++ lib/common/theme/light_theme.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:whats_app_clone/common/extension/custom_theme_extension.dart';
+
 import 'package:whats_app_clone/common/utils/colors.dart';
 
 ThemeData lightTheme() {
@@ -7,6 +9,8 @@ ThemeData lightTheme() {
       // ignore: deprecated_member_use
       backgroundColor: Coloors.backgroundLight,
       scaffoldBackgroundColor: Coloors.backgroundLight,
+      extensions: [CustomThemeExtension.lightMode,
+      ],
       elevatedButtonTheme: ElevatedButtonThemeData(
           style: ElevatedButton.styleFrom(
         backgroundColor: Coloors.greenLight,
