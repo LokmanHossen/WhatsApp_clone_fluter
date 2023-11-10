8912457b94fa40b9443f0d6bea24a6103b58cdd5
LokmanHossen <lokmanhossen230@gmail.com>
2023-11-10 17:08:45 +0600

    Dark Theme

---
+++ lib/common/theme/dark_theme.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+import 'package:whats_app_clone/common/utils/colors.dart';
+
+ThemeData darkTheme() {
+  final ThemeData base = ThemeData.dark();
+  return base.copyWith(
+    // ignore: deprecated_member_use
+    backgroundColor: Coloors.backgroundDark,
+    scaffoldBackgroundColor: Coloors.backgroundDark,
+    elevatedButtonTheme: ElevatedButtonThemeData(
+      style: ElevatedButton.styleFrom(
+        backgroundColor: Coloors.greenDark,
+          foregroundColor: Coloors.backgroundDark,
+          splashFactory: NoSplash.splashFactory,
+          elevation: 0,
+          shadowColor: Colors.transparent,
+      ),
+    ),
+  );
+}
