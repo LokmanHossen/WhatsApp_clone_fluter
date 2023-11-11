6cb5a211f31a9a195a4eab78cc9ba96cdcf21c89
LokmanHossen <lokmanhossen230@gmail.com>
2023-11-11 11:16:28 +0600

    Light BottomSheetTheme

--- lib/common/theme/light_theme.dart
+++ lib/common/theme/light_theme.dart
@@ -6,17 +6,29 @@ import 'package:whats_app_clone/common/utils/colors.dart';
 ThemeData lightTheme() {
   final ThemeData base = ThemeData.light();
   return base.copyWith(
-      // ignore: deprecated_member_use
-      backgroundColor: Coloors.backgroundLight,
-      scaffoldBackgroundColor: Coloors.backgroundLight,
-      extensions: [CustomThemeExtension.lightMode,
-      ],
-      elevatedButtonTheme: ElevatedButtonThemeData(
-          style: ElevatedButton.styleFrom(
+    // ignore: deprecated_member_use
+    backgroundColor: Coloors.backgroundLight,
+    scaffoldBackgroundColor: Coloors.backgroundLight,
+    extensions: [
+      CustomThemeExtension.lightMode,
+    ],
+    elevatedButtonTheme: ElevatedButtonThemeData(
+      style: ElevatedButton.styleFrom(
         backgroundColor: Coloors.greenLight,
         foregroundColor: Coloors.backgroundLight,
         splashFactory: NoSplash.splashFactory,
         elevation: 0,
         shadowColor: Colors.transparent,
-      )));
+      ),
+    ),
+    bottomSheetTheme: const BottomSheetThemeData(
+      backgroundColor: Coloors.backgroundLight,
+      modalBackgroundColor: Coloors.backgroundLight,
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.vertical(
+          top: Radius.circular(30),
+        ),
+      ),
+    ),
+  );
 }
