ad4fc7dd13050558845524bf683a3ef27cdada3c
LokmanHossen <lokmanhossen230@gmail.com>
2023-11-11 11:18:00 +0600

    Add Language List

--- lib/feature/welcome/widgets/language_button.dart
+++ lib/feature/welcome/widgets/language_button.dart
@@ -3,9 +3,82 @@ import 'package:whats_app_clone/common/extension/custom_theme_extension.dart';
 
 import '../../../common/utils/colors.dart';
 
-
 class LanguageButton extends StatelessWidget {
   const LanguageButton({super.key});
+
+  showBottomSheet(context) {
+    return showModalBottomSheet(
+        context: context,
+        builder: (context) {
+          return Padding(
+            padding: const EdgeInsets.symmetric(vertical: 10),
+            child: Column(
+              mainAxisSize: MainAxisSize.min,
+              children: [
+                Container(
+                  height: 4,
+                  width: 20,
+                  decoration: BoxDecoration(
+                    color: context.theme.greyColor!.withOpacity(0.5),
+                    borderRadius: BorderRadius.circular(5),
+                  ),
+                ),
+                const SizedBox(
+                  height: 20,
+                ),
+                Row(
+                  children: [
+                    const SizedBox(width: 20),
+                    IconButton(
+                      onPressed: () => Navigator.of(context).pop(),
+                      splashColor: Colors.transparent,
+                      splashRadius: 22,
+                      iconSize: 22,
+                      padding: EdgeInsets.zero,
+                      constraints: const BoxConstraints(minWidth: 40),
+                      icon: const Icon(
+                        Icons.close_outlined,
+                        color: Coloors.greyDark,
+                      ),
+                    ),
+                    const SizedBox(
+                      width: 10,
+                    ),
+                    const Text(
+                      'App Language',
+                      style: TextStyle(
+                        fontSize: 20,
+                        fontWeight: FontWeight.w500,
+                      ),
+                    ),
+                  ],
+                ),
+                const SizedBox(height: 10),
+                Divider(
+                  color: context.theme.greyColor!.withOpacity(0.3),
+                  thickness: .5,
+                ),
+                RadioListTile(
+                  value: true,
+                  groupValue: true,
+                  onChanged: (value) {},
+                  activeColor: Coloors.greenDark,
+                  title: const Text('Ebglish'),
+                  subtitle: const Text("(Phone's Language)"),
+                ),
+                RadioListTile(
+                  value: true,
+                  groupValue: false,
+                  onChanged: (value) {},
+                  activeColor: Coloors.greenDark,
+                  title: const Text('አማርኛ'),
+                  subtitle: const Text("(Amharic)"),
+                ),
+              ],
+            ),
+          );
+        });
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -13,7 +86,7 @@ class LanguageButton extends StatelessWidget {
       color: context.theme.langButtonColor,
       borderRadius: BorderRadius.circular(50),
       child: InkWell(
-        onTap: () {},
+        onTap: () => showBottomSheet(context),
         borderRadius: BorderRadius.circular(20),
         splashFactory: NoSplash.splashFactory,
         highlightColor: context.theme.langHighilightColor,
@@ -35,9 +108,7 @@ class LanguageButton extends StatelessWidget {
                   color: Coloors.greenDark,
                 ),
               ),
-              SizedBox(
-                width: 10,
-              ),
+              SizedBox(width: 10),
               Icon(
                 Icons.keyboard_arrow_down,
                 color: Coloors.greenDark,
