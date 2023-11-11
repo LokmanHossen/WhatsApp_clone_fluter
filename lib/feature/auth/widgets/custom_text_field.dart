31fda98d4cc1b2a952e1773062c6e451c1638c7c
LokmanHossen <lokmanhossen230@gmail.com>
2023-11-12 00:22:46 +0600

    Custom Login Text Field

---
+++ lib/feature/auth/widgets/custom_text_field.dart
@@ -0,0 +1,50 @@
+import 'package:flutter/material.dart';
+import 'package:whats_app_clone/common/extension/custom_theme_extension.dart';
+import 'package:whats_app_clone/common/utils/colors.dart';
+
+class CustomTextPage extends StatelessWidget {
+  const CustomTextPage({
+    super.key,
+    this.controller,
+    this.hintText,
+    this.readOnly,
+    this.textAlign,
+    this.keyboardType,
+    this.prefixText,
+    this.onTap,
+    this.suffixIcon,
+    this.onChanged,
+  });
+
+  final TextEditingController? controller;
+  final String? hintText;
+  final bool? readOnly;
+  final TextAlign? textAlign;
+  final TextInputType? keyboardType;
+  final String? prefixText;
+  final VoidCallback? onTap;
+  final Widget? suffixIcon;
+  final Function(String)? onChanged;
+
+  @override
+  Widget build(BuildContext context) {
+    return TextFormField(
+      onTap: onTap,
+      controller: controller,
+      readOnly: readOnly ?? false,
+      textAlign: textAlign ?? TextAlign.center,
+      keyboardType: readOnly == null ? keyboardType : null,
+      onChanged: onChanged,
+      decoration: InputDecoration(
+        isDense: true,
+        prefixText: prefixText,
+        suffix: suffixIcon,
+        hintText: hintText,
+        hintStyle: TextStyle(color: context.theme.greyColor),
+        enabledBorder: const UnderlineInputBorder(
+          borderSide: BorderSide(color: Coloors.greenDark, width: 2),
+        ),
+      ),
+    );
+  }
+}
