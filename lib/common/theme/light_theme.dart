750bb37315ade179e364bba2d184772e447f6e45
LokmanHossen <lokmanhossen230@gmail.com>
2023-11-14 15:54:58 +0600

    Update

--- lib/common/theme/light_theme.dart
+++ lib/common/theme/light_theme.dart
@@ -14,6 +14,7 @@ ThemeData lightTheme() {
       CustomThemeExtension.lightMode,
     ],
     appBarTheme: const AppBarTheme(
+      titleTextStyle: TextStyle(fontSize: 18),
       systemOverlayStyle: SystemUiOverlayStyle(
         statusBarColor: Colors.transparent,
         statusBarIconBrightness: Brightness.dark,
