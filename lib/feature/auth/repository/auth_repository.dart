b87cd4095dc7bf6db38229aeb3df353f14f3092f
LokmanHossen <lokmanhossen230@gmail.com>
2023-11-24 21:29:50 +0600

    Verify SmsCode

--- lib/feature/auth/repository/auth_repository.dart
+++ lib/feature/auth/repository/auth_repository.dart
@@ -22,6 +22,30 @@ class AuthRepository {
     required this.auth,
     required this.firestore,
   });
+
+  void verifySmsCode({
+    required BuildContext context,
+    required String smsCodeId,
+    required String smsCode,
+    required bool mounted,
+  }) async {
+    try {
+      final credential = PhoneAuthProvider.credential(
+        verificationId: smsCodeId,
+        smsCode: smsCode,
+      );
+
+      await auth.signInWithCredential(credential);
+      if (!mounted) return;
+      Navigator.of(context).pushNamedAndRemoveUntil(
+        Routes.userInfo,
+        (route) => false,
+      );
+    } on FirebaseAuth catch (e) {
+      showAlertDialog(context: context, message: e.toString());
+    }
+  }
+
   void sendSmsCode({
     required BuildContext context,
     required String phoneNumber,
