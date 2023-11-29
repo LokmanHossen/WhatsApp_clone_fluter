5c474c1e8602decf9349986fd3237981afe03996
LokmanHossen <lokmanhossen230@gmail.com>
2023-11-29 16:21:20 +0600

    User ModelData

--- lib/feature/auth/pages/user_info_page.dart
+++ lib/feature/auth/pages/user_info_page.dart
@@ -2,6 +2,7 @@ import 'dart:io';
 import 'dart:typed_data';
 
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:image_picker/image_picker.dart';
 import 'package:whats_app_clone/common/extension/custom_theme_extension.dart';
 import 'package:whats_app_clone/common/helper/show_alert_dialog.dart';
@@ -9,19 +10,40 @@ import 'package:whats_app_clone/common/utils/colors.dart';
 import 'package:whats_app_clone/common/widgets/custom_elevated_button.dart';
 import 'package:whats_app_clone/common/widgets/custom_icon_button.dart';
 import 'package:whats_app_clone/common/widgets/short_h_bar.dart';
+import 'package:whats_app_clone/feature/auth/controller/auth_controller.dart';
 import 'package:whats_app_clone/feature/auth/pages/image_picker_page.dart';
 import 'package:whats_app_clone/feature/auth/widgets/custom_text_field.dart';
 
-class UserInfoPage extends StatefulWidget {
+class UserInfoPage extends ConsumerStatefulWidget {
   const UserInfoPage({super.key});
 
   @override
-  State<UserInfoPage> createState() => _UserInfoPageState();
+  ConsumerState<UserInfoPage> createState() => _UserInfoPageState();
 }
 
-class _UserInfoPageState extends State<UserInfoPage> {
+class _UserInfoPageState extends ConsumerState<UserInfoPage> {
   File? imageCamera;
   Uint8List? imageGallery;
+
+  late TextEditingController usernameController;
+
+  saveUSerDataToFirebase() {
+    String username = usernameController.text;
+    if (username.isEmpty) {
+      return showAlertDialog(
+          context: context, message: 'Please provide a username');
+    } else if (username.length < 3 || username.length > 20) {
+      return showAlertDialog(
+          context: context,
+          message: 'A username length should be between 3-20');
+    }
+    ref.read(authControllerProvider).saveUserInfoToFirestore(
+          username: username,
+          profileImage: imageCamera ?? imageGallery ?? '',
+          context: context,
+          mounted: mounted,
+        );
+  }
 
   imagePickerTypeBottomSheet() {
     return showModalBottomSheet(
@@ -126,6 +148,18 @@ class _UserInfoPageState extends State<UserInfoPage> {
   }
 
   @override
+  void initState() {
+    usernameController = TextEditingController();
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    usernameController.dispose();
+    super.dispose();
+  }
+
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
@@ -189,8 +223,9 @@ class _UserInfoPageState extends State<UserInfoPage> {
             Row(
               children: [
                 const SizedBox(width: 20),
-                const Expanded(
+                Expanded(
                   child: CustomTextPage(
+                    controller: usernameController,
                     hintText: 'Type your name here',
                     textAlign: TextAlign.left,
                     autoFocus: true,
@@ -208,7 +243,7 @@ class _UserInfoPageState extends State<UserInfoPage> {
       ),
       floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
       floatingActionButton: CustomElevatedButton(
-        onPressed: () {},
+        onPressed: saveUSerDataToFirebase,
         text: 'NEXT',
         buttonWidth: 90,
       ),
