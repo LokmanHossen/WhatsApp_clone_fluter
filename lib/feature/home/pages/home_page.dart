9f6415e464025a9f3999d5b554101da209a7a7c0
LokmanHossen <lokmanhossen230@gmail.com>
2023-12-05 23:57:54 +0600

    HomePage

--- lib/feature/home/pages/home_page.dart
+++ lib/feature/home/pages/home_page.dart
@@ -1,12 +1,46 @@
 import 'package:flutter/material.dart';
+import 'package:whats_app_clone/common/widgets/custom_icon_button.dart';
+import 'package:whats_app_clone/feature/home/chat_home_page.dart';
+import 'package:whats_app_clone/feature/home/pages/call_home_page.dart';
+import 'package:whats_app_clone/feature/home/pages/status_home_page.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(),
+    return DefaultTabController(
+      length: 3,
+      child: Scaffold(
+        appBar: AppBar(
+          title: const Text(
+            'WhatsApp',
+            style: TextStyle(letterSpacing: 1),
+          ),
+          elevation: 1,
+          actions: [
+            CustomIconButton(onTap: () {}, icon: Icons.search),
+            CustomIconButton(onTap: () {}, icon: Icons.more_vert),
+          ],
+          bottom: const TabBar(
+            indicatorWeight: 3,
+            labelStyle: TextStyle(fontWeight: FontWeight.bold),
+            splashFactory: NoSplash.splashFactory,
+            tabs: [
+              Tab(text: 'CHATS'),
+              Tab(text: 'STATUS'),
+              Tab(text: 'CALLS'),
+            ],
+          ),
+        ),
+        body: const TabBarView(
+          children: [
+            ChatHomePage(),
+            StatusHomePage(),
+            CallHomePage(),
+          ],
+        ),
+      ),
     );
   }
 }
