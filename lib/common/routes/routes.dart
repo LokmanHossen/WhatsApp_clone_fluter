d068c2042893e7d63574d0226739ec9fbb9d87c4
LokmanHossen <lokmanhossen230@gmail.com>
2023-12-26 11:22:07 +0600

    Route ProfilePage

--- lib/common/routes/routes.dart
+++ lib/common/routes/routes.dart
@@ -4,9 +4,11 @@ import 'package:whats_app_clone/feature/auth/pages/login_page.dart';
 import 'package:whats_app_clone/feature/auth/pages/user_info_page.dart';
 import 'package:whats_app_clone/feature/auth/pages/verification_page.dart';
 import 'package:whats_app_clone/feature/chat/pages/chat_page.dart';
+import 'package:whats_app_clone/feature/chat/pages/profile_page.dart';
 import 'package:whats_app_clone/feature/contact/page/contact_page.dart';
 import 'package:whats_app_clone/feature/home/pages/home_page.dart';
 import 'package:whats_app_clone/feature/welcome/pages/welcome_page.dart';
+import 'package:page_transition/page_transition.dart';
 
 class Routes {
   static const String welcome = 'welcome';
@@ -16,6 +18,7 @@ class Routes {
   static const String home = 'home';
   static const String contact = 'contact';
   static const String chat = 'chat';
+  static const String profile = 'profile';
 
   static Route<dynamic> onGenerateRoute(RouteSettings settings) {
     switch (settings.name) {
@@ -54,7 +57,16 @@ class Routes {
       case chat:
         final UserModel user = settings.arguments as UserModel;
         return MaterialPageRoute(
-          builder: (context) =>  ChatPage(user: user,),
+          builder: (context) => ChatPage(
+            user: user,
+          ),
+        );
+      case profile:
+        final UserModel user = settings.arguments as UserModel;
+        return PageTransition(
+          child: ProfilePage(user: user),
+          type: PageTransitionType.fade,
+          duration: const Duration(microseconds: 800),
         );
       default:
         return MaterialPageRoute(
