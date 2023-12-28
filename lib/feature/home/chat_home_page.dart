ac8bac11dffc0c8225b728940ce7ffa551475878
LokmanHossen <lokmanhossen230@gmail.com>
2023-12-28 13:38:14 +0600

    ChatPage,UserChatList

--- lib/feature/home/chat_home_page.dart
+++ lib/feature/home/chat_home_page.dart
@@ -1,7 +1,14 @@
+import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:intl/intl.dart';
+import 'package:whats_app_clone/common/extension/custom_theme_extension.dart';
+import 'package:whats_app_clone/common/models/last_message_model.dart';
 import 'package:whats_app_clone/common/routes/routes.dart';
+import 'package:whats_app_clone/common/utils/colors.dart';
+import 'package:whats_app_clone/feature/chat/controllers/chat_controller.dart';
 
-class ChatHomePage extends StatelessWidget {
+class ChatHomePage extends ConsumerWidget {
   const ChatHomePage({super.key});
 
   navigateToContactPage(context) {
@@ -9,10 +16,58 @@ class ChatHomePage extends StatelessWidget {
   }
 
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetRef ref) {
     return Scaffold(
-      body: const Center(
-        child: Text('Chat Home PAge'),
+      body: StreamBuilder<List<LastMessageModel>>(
+        stream: ref.watch(chatControllerProvider).getAllLastMessageList(),
+        builder: (_, snapshot) {
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return const Center(
+              child: CircularProgressIndicator(
+                color: Coloors.greenDark,
+              ),
+            );
+          }
+          return ListView.builder(
+            itemCount: snapshot.data == null ? 0 : snapshot.data!.length,
+            // snapshot.data!.length,
+            // snapshot.data == null ? 0 : snapshot.data!.length,
+            shrinkWrap: true,
+            itemBuilder: (context, index) {
+              final lastMessageData = snapshot.data![index];
+              return ListTile(
+                title: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Text(lastMessageData.username),
+                    Text(
+                      DateFormat.Hm().format(lastMessageData.timeSent),
+                      style: TextStyle(
+                        fontSize: 13,
+                        color: context.theme.greyColor,
+                      ),
+                    ),
+                  ],
+                ),
+                subtitle: Padding(
+                  padding: const EdgeInsets.only(top: 3),
+                  child: Text(
+                    lastMessageData.lastMessage,
+                    maxLines: 1,
+                    overflow: TextOverflow.ellipsis,
+                    style: TextStyle(color: context.theme.greyColor),
+                  ),
+                ),
+                leading: CircleAvatar(
+                  backgroundImage: CachedNetworkImageProvider(
+                    lastMessageData.profileImageUrl,
+                  ),
+                  radius: 24,
+                ),
+              );
+            },
+          );
+        },
       ),
       floatingActionButton: FloatingActionButton(
         onPressed: () => navigateToContactPage(context),
